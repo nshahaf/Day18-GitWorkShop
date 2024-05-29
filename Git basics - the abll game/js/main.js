d0fd0c92fba448399a71c7aeec705ec4717d3fb3
nShahaf <shahafnoked@gmail.com>
2024-05-29 16:39:17 +0300

    added random color for the ball on click

--- Git basics - the abll game/js/main.js
+++ Git basics - the abll game/js/main.js
@@ -4,11 +4,12 @@ var ballDiameter = 100
 function onBallClick(elBall) {
     console.log('clicked!')
     
-    ballDiameter >= 400? ballDiameter = 100 : ballDiameter +=50
+    ballDiameter >= 400? ballDiameter = 100 : ballDiameter += getRandomInt(20,61)
 
     elBall.style.height = ballDiameter + 'px'
     elBall.style.width = ballDiameter + 'px'
 
+    elBall.style.backgroundColor = getRandomColor()
     elBall.innerText = ballDiameter
 
 }
