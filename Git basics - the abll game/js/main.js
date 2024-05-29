31978c15d05c46ded8e784b65d451d8fe39d29d2
nShahaf <shahafnoked@gmail.com>
2024-05-29 18:13:46 +0300

    add ball 3 functionality

--- Git basics - the abll game/js/main.js
+++ Git basics - the abll game/js/main.js
@@ -1,18 +1,51 @@
 'use strict'
-var ballDiameter = [100,100]
+var serialId = 0
+var balls = [
+    {diameter: 100, backgroundColor: 'yellow'},
+    {diameter: 100, backgroundColor: 'yellow'},
+    {diameter: 100, backgroundColor: 'yellow'},
+    {diameter: 100, backgroundColor: 'yellow'},
+    {diameter: 100, backgroundColor: 'yellow'}
+]
 
 function onBallClick(elBall,idx) {
     console.log('clicked!')
+    var currBall = balls[idx]
     
-    ballDiameter[idx] >= 400? ballDiameter[idx] = 100 : ballDiameter[idx] += getRandomInt(20,61)
-   
+    //diameter
+    currBall.diameter >= 400? currBall.diameter = 100 : currBall.diameter += getRandomInt(20,61)
+    elBall.style.height = currBall.diameter + 'px'
+    elBall.style.width = currBall.diameter + 'px'
 
-    elBall.style.height = ballDiameter[idx] + 'px'
-    elBall.style.width = ballDiameter[idx] + 'px'
+    //random color
+    var randColor = getRandomColor()
+    currBall.backgroundColor = randColor
 
-    elBall.style.backgroundColor = getRandomColor()
-    elBall.innerText = ballDiameter[idx]
+    elBall.style.backgroundColor = randColor
+    elBall.innerText = currBall.diameter
     
-    elBall.innerText = ballDiameter[idx]
+}
 
+function swapBall1and2() {
+    var ballIdx1 = balls.splice(1,1)
+    balls.unshift(ballIdx1[0])
+
+    var elBall1 = document.querySelector('.ball1')
+    var elBall2 = document.querySelector('.ball2') 
+    
+        elBall1.style.width = balls[0].diameter
+        elBall1.style.height = balls[0].diameter
+        elBall1.innerText = balls[0].diameter
+        elBall1.style.backgroundColor = balls[0].backgroundColor
+        
+        elBall2.style.width = balls[1].diameter
+        elBall2.style.height = balls[1].diameter
+        elBall2.innerText = balls[1].diameter
+        elBall2.style.backgroundColor = balls[1].backgroundColor
+
+    
+
+    
+    
+    
 }
