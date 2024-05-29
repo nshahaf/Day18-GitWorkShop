cd7bc96db92a00da44f4a917d8dbedd2a527091a
nShahaf <shahafnoked@gmail.com>
2024-05-29 16:00:16 +0300

    Adde functionality to ball click + more styling

--- Git basics - the abll game/js/main.js
+++ Git basics - the abll game/js/main.js
@@ -1,5 +1,17 @@
 'use strict'
+var ball = { h: 100, w: 100 }
 
-function onBallClick() {
-    console.log('ball clicked!')
+function onBallClick(elBall) {
+    console.log('clicked!')
+    // console.log('elBall:', elBall)
+    // console.log('elBall.innerText:',elBall.innerText)
+    // console.log('elBall.style:',elBall.style)
+    ball.h += 50
+    ball.w += 50
+
+    elBall.style.height = ball.h + 'px'
+    elBall.style.width = ball.w + 'px'
+
+    elBall.innerText = ball.h
+
 }
